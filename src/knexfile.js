22c459dbce44ee771f1e78980f3478b7576fc0d2
Henrique Souza NGI <henrique.souza@ngi.com.br>
2021-10-22 08:47:09 -0300

    chore: create knexfile

---
+++ src/knexfile.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+
+module.exports = {
+
+  client: 'mysql',
+  connection: {
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
+
+};
